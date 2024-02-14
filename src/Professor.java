47d832ed825efeb9ae0dfd4a62ea448846dcf820
kpossoli <153238497+kpossoli@users.noreply.github.com>
2024-02-13 22:02:57 -0300

    Commit classe Professor

---
+++ src/Professor.java
@@ -0,0 +1,38 @@
+public class Professor {
+    private String nome;
+    private int idade;
+    private int tempoTrabalho;
+
+    public Professor(String nome, int idade, int tempoTrabalho) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tempoTrabalho = tempoTrabalho;
+    }
+
+    public Professor(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getTempoTrabalho() {
+        return tempoTrabalho;
+    }
+
+    public void setTempoTrabalho(int tempoTrabalho) {
+        this.tempoTrabalho = tempoTrabalho;
+    }
+}
+
