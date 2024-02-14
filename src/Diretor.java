3b3746c65a515e926953c1170e866e2898fb14eb
Leonardo Vieira <leonardovieira1993@gmail.com>
2024-02-14 12:58:31 -0300

    Adicionados Getters e Setters à Classe Diretor

--- src/Diretor.java
+++ src/Diretor.java
@@ -2,5 +2,22 @@ public class Diretor extends Funcionario{
 
     private int tempoTrabalho;
 
+    public Diretor() {
+
+    }
+
+    public Diretor(String nome, double salario, int tempoTrabalho) {
+        super(nome, salario);
+        this.tempoTrabalho = tempoTrabalho;
+    }
+
+    public int getTempoTrabalho() {
+        return tempoTrabalho;
+    }
+
+    public void setTempoTrabalho(int tempoTrabalho) {
+        this.tempoTrabalho = tempoTrabalho;
+    }
+
 }
 
