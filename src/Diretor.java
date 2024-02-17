96e33688e7cb2dd2e651033a53759acbb5dfb8fe
Marcos Vinicios <mvdias1268@gmail.com>
2024-02-17 14:56:52 -0300

    Adicionado id no diretor

--- src/Diretor.java
+++ src/Diretor.java
@@ -1,5 +1,8 @@
 public class Diretor extends Funcionario{
 
+    private static int idGeral = 0;
+    private int idDiretor;
+    private String nome;
     private int tempoTrabalho;
 
     public Diretor() {
@@ -9,6 +12,17 @@ public class Diretor extends Funcionario{
     public Diretor(String nome, double salario, int tempoTrabalho) {
         super(nome, salario);
         this.tempoTrabalho = tempoTrabalho;
+
+        idGeral += 1;
+        setIdDiretor(idGeral);
+    }
+
+    public int getIdDiretor() {
+        return idDiretor;
+    }
+
+    public void setIdDiretor(int idDiretor) {
+        this.idDiretor = idDiretor;
     }
 
     public int getTempoTrabalho() {
